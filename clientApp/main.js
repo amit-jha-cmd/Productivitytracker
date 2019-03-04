1664d713c12b94c0e87e72058fcbeb184e036db1
Amit jha <amit.jha6700@gmail.com>
2019-03-04 19:02:01 +0530

    tabs added

--- clientApp/main.js
+++ clientApp/main.js
@@ -1,11 +1,10 @@
 const electron = require('electron');
-
 const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
     //create the browser window
-    let win = new BrowserWindow({width: 800, height: 600});
-
+    let win = new BrowserWindow({width: 900, height: 800});
+    win.setMenu(null);
     //and load the index.html of the app.
     win.loadFile('index.html');
 

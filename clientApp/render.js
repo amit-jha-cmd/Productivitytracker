27f002c88ae9bb46d45104f9477c6b74ac76ca88
Amit jha <amit.jha6700@gmail.com>
2019-03-06 01:17:25 +0530

    chart added

--- clientApp/render.js
+++ clientApp/render.js
@@ -1,5 +1,51 @@
-'use strict'
+new Chart(document.getElementById("countries"), {
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Predicted world population (millions) in 2050'
+      },
+      legend: {
+        display: false
+      },
+    }
+});
 
-window.$ = window.jQuery = require('jquery')
-window.Tether = require('tether')
-window.Bootstrap = require('bootstrap')
+            // // pie chart data
+            // var pieData = [
+            //     {
+            //         value: 20,
+            //         color:"#878BB6"
+            //     },
+            //     {
+            //         value : 40,
+            //         color : "#4ACAB4"
+            //     },
+            //     {
+            //         value : 10,
+            //         color : "#FF8153"
+            //     },
+            //     {
+            //         value : 30,
+            //         color : "#FFEA88"
+            //     }
+            // ];
+            // // pie chart options
+            // var pieOptions = {
+            //      segmentShowStroke : false,
+            //      animateScale : true
+            // }
+            // // get pie chart canvas
+            // var countries= document.getElementById("countries").getContext("2d");
+            // // draw pie chart
+            // new Chart(countries).Pie(pieData, pieOptions);

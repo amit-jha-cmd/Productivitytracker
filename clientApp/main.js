d71aee9740dbb33ed8fd79ee53376ef6819c2960
devbihari <amit.jha6700@gmail.com>
2019-03-08 22:21:59 +0530

    fixed minor bugs

--- clientApp/main.js
+++ clientApp/main.js
@@ -3,11 +3,14 @@ const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
     //create the browser window
-    let win = new BrowserWindow({width: 900, height: 500, frame: false, resizable: false});
+    let win = new BrowserWindow({width: 900,
+                                 height: 500, 
+                                 frame: false, 
+                                 resizable: false
+                                });
     // win.setMenu(null);
     //and load the index.html of the app.
     win.loadFile('index.html');
-
     win.on('closed', () => {
         //dereference the window object, usually you would store the windows
         //in an array if your app supports multi windows, this is the time

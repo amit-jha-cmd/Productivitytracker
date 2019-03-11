8d1c20e59f0edcedc690488db6e5e12170dfb5a7
Amit jha <amit.jha6700@gmail.com>
2019-03-11 20:55:38 +0530

    log entry page added

---
+++ clientApp/log.js
@@ -0,0 +1,9 @@
+setInterval(()=>{
+    var t = new Date();
+    var hour = t.getHours();
+    var min = t.getMinutes();
+    var sec = t.getSeconds();
+
+    var labelTime = hour + ":" + min + ":" + sec; 
+    document.getElementById("time").innerHTML = labelTime;
+})
--- clientApp/render.js
+++ clientApp/render.js
@@ -1,5 +1,4 @@
 const {BrowserWindow} = require('electron').remote;
 document.getElementById('exit').addEventListener('click', ()=>{
-    var window = BrowserWindow.getFocusedWindow();
     window.close();
 });
